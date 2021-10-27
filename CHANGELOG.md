95ca5b7f0202bd271c038d9dc363dccf1dbbe0f1
apps-auth <admin@appsauth.com>
2021-10-27 14:41:04 -0300

    1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 1.0.1
+Fix example
+
 # 1.0.0
 
 Initial release
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'pages/my_app.dart';
 
-String? applicationDocumentsDirectory;
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  applicationDocumentsDirectory =
-      (await getApplicationDocumentsDirectory()).path;
-
+void main() {
   runApp(const MyApp());
 }
